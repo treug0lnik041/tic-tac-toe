cbd29e18cd6dcfb4306fc0facb711e8cd91f7b5f
treug0lnik041 <treug0lnik@yahoo.com>
2022-12-07 17:48:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## tic-tac-toe
+---
+
+This is a example project directly from [Anchor book](https://book.anchor-lang.com/anchor_in_depth/milestone_project_tic-tac-toe.html "Link to the project chapter")
---
+++ tests/tic-tac-toe.ts
@@ -0,0 +1,89 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
+import { TicTacToe } from "../target/types/tic_tac_toe";
+
+describe("tic-tac-toe", () => {
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.AnchorProvider.env());
+
+    const program = anchor.workspace.TicTacToe as Program<TicTacToe>;
+
+    it("setup game!", async () => {
+        const gameKeypair = anchor.web3.Keypair.generate();
+        const playerOne = (program.provider as anchor.AnchorProvider).wallet;
+        const playerTwo = anchor.web3.Keypair.generate();
+
+        await program.methods
+            .setupGame(playerTwo.publicKey)
+            .accounts({
+                game: gameKeypair.publicKey,
+                playerOne: playerOne.publicKey,
+            })
+            .signers([gameKeypair])
+            .rpc();
+
+        let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+        expect(gameState.turn).to.equal(1);
+        expect(gameState.players).to.eql([playerOne.publicKey, playerTwo.publicKey]);
+        expect(gameState.state).to.eql({ active: {}});
+        expect(gameState.board).to.eql([
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+        ]);
+    })
+
+    it("play", async() => {
+        const gameKeypair = anchor.web3.Keypair.generate();
+        const playerOne = (program.provider as anchor.AnchorProvider).wallet;
+        const playerTwo = anchor.web3.Keypair.generate();
+
+        await program.methods
+            .setupGame(playerTwo.publicKey)
+            .accounts({
+                game: gameKeypair.publicKey,
+                playerOne: playerOne.publicKey,
+            })
+            .signers([gameKeypair])
+            .rpc();
+
+        let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+        expect(gameState.turn).to.equal(1);
+        expect(gameState.players).to.eql([playerOne.publicKey, playerTwo.publicKey]);
+        expect(gameState.state).to.eql({active: {}});
+        expect(gameState.board).to.eql([
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+        ]);
+
+        await play(program, gameKeypair.publicKey, playerOne, { row: 0, column: 0}, 2, { active: {} }, [
+            [{ x: {} }, null, null],
+            [null, null, null],
+            [null, null, null],
+        ]);
+
+        await play(program, gameKeypair.publicKey, playerTwo, { row: 1, column: 0 }, 3, { active: {}}, [
+            [{ x: {} }, null, null],
+            [{ o: {}}, null, null],
+            [null, null, null],
+        ]);
+    })
+
+    async function play(program: Program<TicTacToe>, game, player, tile, expectedTurn, expectedGameState, expectedBoard) {
+        await program.methods
+            .play(tile)
+            .accounts({
+                player: player.publicKey,
+                game,
+            })
+            .signers(player instanceof (anchor.Wallet as any) ? []: [player])
+            .rpc();
+
+            const gameState = await program.account.game.fetch(game);
+            expect(gameState.turn).to.equal(expectedTurn);
+            expect(gameState.state).to.eql(expectedGameState);
+            expect(gameState.board).to.eql(expectedBoard);
+    }
+});
